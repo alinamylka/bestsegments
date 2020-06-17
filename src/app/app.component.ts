3a72db3ffa978ba7761789d688bbed06383aa581
amylka <alina_maria.mylka@sbb.ch>
2020-06-17 19:05:30 +0200

    add some styles to athlete and change the name of the app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import {AuthorizeService} from './authorize/authorize.service';
     styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-    title = 'bestsegments';
+    title = 'BEST SEGMENTS APP';
 
     constructor(private authorisationService: AuthorizeService) {
     }
