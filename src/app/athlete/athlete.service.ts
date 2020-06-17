c88769f814989e758f9392adf50dbc7625958170
amylka <alina_maria.mylka@sbb.ch>
2020-06-17 16:27:19 +0200

    add interceptor token

--- src/app/athlete/athlete.service.ts
+++ src/app/athlete/athlete.service.ts
@@ -1,8 +1,7 @@
 import {Injectable} from '@angular/core';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {Athlete} from '../athlete';
-import {AuthorizeService} from '../authorize/authorize.service';
 
 @Injectable({
     providedIn: 'root'
