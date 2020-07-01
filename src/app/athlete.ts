770bfaa1ce22564ea719eaa44df87b6eaca0d032
amylka <alina_maria.mylka@sbb.ch>
2020-07-01 07:29:11 +0200

    added best efforts with hard coded date for a segment with hard coded date, and simple navigation from segment to segment effort

--- src/app/athlete.ts
+++ src/app/athlete.ts
@@ -1,24 +1,25 @@
 import {AthleteDto} from './athlete/athleteDto';
 
 export class Athlete {
-    constructor(private username: string,
+    constructor(private id: number,
+                private username: string,
                 private firstname: string,
                 private lastname: string,
-                private  city: string,
-                private   state: string,
-                private   country: string,
-                private   sex: string,
-                private  premium: boolean,
+                private city: string,
+                private state: string,
+                private country: string,
+                private sex: string,
+                private premium: boolean,
                 private summit: boolean,
                 private createdAt: string,
-                private  updatedAt: string,
+                private updatedAt: string,
                 private profileMedium: string,
                 private profile: string) {
     }
 
 
     public static init(dto: AthleteDto): Athlete {
-        return new Athlete(dto.username, dto.firstname, dto.lastname, dto.city,
+        return new Athlete(dto.id, dto.username, dto.firstname, dto.lastname, dto.city,
             dto.state, dto.country, dto.sex, dto.premium, dto.summit, dto.created_at,
             dto.updated_at, dto.profile_medium, dto.profile);
     }
