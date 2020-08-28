1530c2f3a028eafc0d0c416209397d848f401a99
u216116 <alina_maria.mylka@sbb.ch>
2020-08-28 18:42:06 +0200

    fixed sync service

--- src/app/sync/sync.service.ts
+++ src/app/sync/sync.service.ts
@@ -3,11 +3,11 @@ import {AthleteStravaService} from '../athlete/athlete-strava.service';
 import {AthleteDto} from '../athlete/athleteDto';
 import {Athlete} from '../model/athlete';
 import {ChallengesStoreService} from '../challenges/challenges-store.service';
-import {SegmentStravaService} from '../segment/segment-strava.serivce';
 import {SegmentEffortStravaService} from '../segment.effort/segment-effort-strava.service';
 import {Challenge} from '../model/challenge';
 import {AthleteStoreService} from '../athlete/athlete-store.service';
 import {SegmentEffortStoreService} from '../segment.effort/segment-effort-store.service';
+import {SegmentStoreService} from '../segment/segment-store.serivce';
 
 @Injectable({
     providedIn: 'root'
@@ -16,7 +16,7 @@ export class SyncService {
     constructor(private athleteStravaService: AthleteStravaService,
                 private athleteStoreService: AthleteStoreService,
                 private challengeService: ChallengesStoreService,
-                private segmentService: SegmentStravaService,
+                private segmentStoreService: SegmentStoreService,
                 private effortStravaService: SegmentEffortStravaService,
                 private effortStoreService: SegmentEffortStoreService) {
     }
@@ -25,7 +25,7 @@ export class SyncService {
         this.athleteStravaService.athleteInfo()
             .subscribe((data: AthleteDto) => {
                 Athlete.init(data).save(this.athleteStoreService)
-                    .challenges(this.challengeService, this.segmentService, this.athleteStoreService, this.effortStravaService)
+                    .challenges(this.challengeService, this.segmentStoreService, this.athleteStoreService, this.effortStravaService)
                     .subscribe(challenges => Challenge.addEfforts(this.effortStravaService, this.effortStoreService, challenges));
             });
     }
