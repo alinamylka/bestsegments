6bf2b8bd8feebc005b7f24325c22b015538ee1ea
u216116 <alina_maria.mylka@sbb.ch>
2020-09-07 08:50:43 +0200

    change header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import {Component} from '@angular/core';
 })
 export class AppComponent {
     title = 'Best Segments';
-    subtitle = 'Alina Mylka';
+    subtitle = 'ALINA MYLKA';
 
     constructor() {
     }
